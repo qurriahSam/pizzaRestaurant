d84859a492ff6fa560fb910d611a2e80d4367878
90150N <mugirethe1@gmail.com>
2022-03-20 23:48:13 +0300

    ft: add get total function and create receipt constructor

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     cheese: 200,
     pepperoni: 300,
     stuffed: 200,
-    glutenFree: 300,
+    gluttenFree: 300,
     crispy: 400,
   };
 
@@ -24,6 +24,15 @@ $(document).ready(function () {
     e.preventDefault();
     let userChoices = [];
     let isValid = true;
+    let total = 0;
+
+    function Receipt(price, crust, toppings, size, amount) {
+      this.price = price;
+      this.crust = crust;
+      this.toppings = toppings;
+      this.size = size;
+      this.amount = amount;
+    }
 
     let getChoice = (menuChoice) => {
       menuChoice.map((choice) => {
@@ -43,11 +52,25 @@ $(document).ready(function () {
 
     let errorReport = (isValid) => {
       if (!isValid) {
-        $(".modal-body").append("Kindly fill all Sections.");
+        $("#message").text("Kindly fill all Sections.");
+      } else {
+        getTotal(userChoices);
       }
     };
 
+    let getTotal = (choicesArr) => {
+      let tots = 0;
+      for (let i = 0; i < choicesArr.length - 1; i++) {
+        tots += prices[choicesArr[i]];
+      }
+      total = tots * parseInt(choicesArr[3]);
+    };
     getChoice(menuChoice);
-    console.log(isValid);
+
+    let [size, toppings, crust, amount] = userChoices;
+
+    console.log(size, toppings, crust, amount);
+
+    console.log(total);
   });
 });
